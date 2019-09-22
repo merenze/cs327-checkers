6774095ffc6a22d0dd5778d03d55870d1cf95212
Matthew Renze <merenze@gmail.com>
2019-09-22 11:46:22 -0500

    Remove extraneous files. Add libutil.h and libutil.c.

---
+++ libutil.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int iswhitespace(char);
+
+void skipline(char*);
+
+const char whitespace[] = " \n\t\r";
+
+void next_token(char[] token, int size) {
+	char ch;	// Char being operated on.
+
+	// Skip comments and leading whitespace.
+	while (iswhitespace(ch == getchar()) || ch == '#') {
+		if (ch == '#') {
+			skipline(&ch);
+		} // Else move cursor to next char; done in loop.
+	}
+
+	token[0] = ch;	// We already have the first char of our token.
+	int i;		// Declared outside loop in order to facilitate adding of terminating null char.
+	
+	// Main loop to load chars into token.
+	for (i = 1; i < size - 1 && !iswhitespace(ch = getchar()) && ch != EOF; i++) {
+		// Since comments continue until newline, they always denote the end of a token.
+		if (ch == '#') {
+			skipline(&ch);
+			i++;
+			break;
+		}
+		token[i] = ch;
+		
+	}
+	token[i] = 0;
+}
+
+int equals(char s1[] char s2[]) {
+	if (sizeof(s1) != sizeof(s2)) {
+		return 0;
+	}
+	for (int i = 0; s1[i] != 0 && s2[i] != 0; i++) {
+		if (s1[i] != s2[i]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int iswhitespace(char ch) {
+	for (int i = 0; whitespace[i] != 0; i++) {
+		if (ch == whitespace[i]) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void skipline(char* ch_ptr) {
+	while ((*ch = getchar()) != '\n');
+}
---
+++ libutil.h
@@ -0,0 +1,30 @@
+/*
+ * libutil.h
+ *
+ * Contains functions for managing standard input.
+ *
+ * Matthew Renze
+ */
+
+#ifndef	LIBUTIL_H
+#define	LIBUTIL_H
+
+/*
+ * Get the next complete token from standard input.
+ * Tokens are delimited by whitespace: space, newline, tab, and carriage return.
+ * Comments (everything between '#' and newline) are ignored.
+ *
+ * char[]	Array in which to store the token.
+ * int		Maximum allowed length of token. Usually the length of the array.
+ */
+void next_token(char[], int);
+
+/*
+ * Test whether two strings are equal by comparing each element.
+ *
+ * char[]	String to test.
+ * char[]	String to test.
+ */
+void equals(char[], char[]);
+
+#endif
